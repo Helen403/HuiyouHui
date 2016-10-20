3ec33c44ed23290b8473c784dc777a34fff86ab3
helen <852568775@qq.com>
2016-10-20 10:40:19 +0800

    第五次修改

--- app/src/main/java/com/huiyouhui/activity/CardManagerActivity.java
+++ app/src/main/java/com/huiyouhui/activity/CardManagerActivity.java
@@ -1,19 +1,31 @@
 package com.huiyouhui.activity;
 
+import com.huiyouhui.R;
+import com.huiyouhui.adapter.CardManagerAdapter;
+import com.huiyouhui.bean.CardManagerBean;
+import com.huiyouhui.lib.MyRecycleView.MyLinearLayoutManager;
 import com.huiyouhui.lib.base.BaseActivity;
+import com.huiyouhui.lib.base.MyBaseRecycleAdapter;
+import com.huiyouhui.lib.custemview.MyRecycleView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2016/10/20.
  */
 public class CardManagerActivity extends BaseActivity {
+    MyRecycleView myRecycleView;
+    CardManagerAdapter cardManagerAdapter;
+
     @Override
     public int getContentView() {
-        return 0;
+        return R.layout.activity_card_manager;
     }
 
     @Override
     public void findViews() {
-
+        setTitle("卡卷管理");
+        myRecycleView = (MyRecycleView) findViewById(R.id.myrecycleview);
     }
 
     @Override
@@ -23,6 +35,41 @@ public class CardManagerActivity extends BaseActivity {
 
     @Override
     public void setListeners() {
+
+    }
+
+
+    @Override
+    protected void onAttachMyRecycleViewAdapter() {
+        super.onAttachMyRecycleViewAdapter();
+        // 使用重写后的线性布局管理器
+        MyLinearLayoutManager manager = new MyLinearLayoutManager(this);
+        myRecycleView.setLayoutManager(manager);
+
+        cardManagerAdapter = new CardManagerAdapter(this, myRecycleView);
+        myRecycleView.setAdapter(cardManagerAdapter);
+        // 刷新
+        myRecycleView.setRefresh(true);
+
+        cardManagerAdapter.setOnRefresh(new MyBaseRecycleAdapter.OnRefresh() {
+            @Override
+            public void onRefresh() {
+                ArrayList<CardManagerBean> data = new ArrayList<CardManagerBean>();
+                for (int i = 0; i < 5; i++) {
+                    data.add(new CardManagerBean());
+                }
+                cardManagerAdapter.setRefresh(data);
+            }
+
+            @Override
+            public void onAddData() {
+                ArrayList<CardManagerBean> data = new ArrayList<CardManagerBean>();
+                for (int i = 0; i < 1; i++) {
+                    data.add(new CardManagerBean());
+                }
+                cardManagerAdapter.setAddData(data);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/huiyouhui/adapter/CardManagerAdapter.java
@@ -0,0 +1,27 @@
+package com.huiyouhui.adapter;
+
+import android.content.Context;
+
+import com.huiyouhui.R;
+import com.huiyouhui.bean.CardManagerBean;
+import com.huiyouhui.lib.base.MyBaseRecycleAdapter;
+import com.huiyouhui.lib.custemview.MyRecycleView;
+
+/**
+ * Created by Administrator on 2016/10/20.
+ */
+public class CardManagerAdapter extends MyBaseRecycleAdapter<CardManagerBean> {
+    public CardManagerAdapter(Context context, MyRecycleView mRecyclerView) {
+        super(context, mRecyclerView);
+    }
+
+    @Override
+    public int getContentView() {
+        return R.layout.item_card_manager;
+    }
+
+    @Override
+    public void onInitView(RecycleViewHolder holder, CardManagerBean cardManagerBean, int position) {
+
+    }
+}
