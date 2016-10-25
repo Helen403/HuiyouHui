561076441818e2e4403c8a9dc5af9e1c466ace62
helen <852568775@qq.com>
2016-10-25 15:16:23 +0800

    添加LRecycleView

--- app/src/main/java/com/huiyouhui/fragment/RecommendMemberFragment.java
+++ app/src/main/java/com/huiyouhui/fragment/RecommendMemberFragment.java
@@ -65,7 +65,6 @@ public class RecommendMemberFragment extends BaseFragment {
                     data.add(new RecommendMemberBean());
                 }
                 recommendMemberAdapter.setAddData(data);
-
             }
         });
     }
